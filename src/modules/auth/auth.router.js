ae9022764446609571c3e00f35b7366a0da2b2b9
sarrah171996 <sarrahmahmoud714@gmail.com>
2024-04-05 16:31:03 +0200

    u

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -4,9 +4,7 @@ import * as validators from './auth.validation.js'
 import { validation } from "../../middleware/validation.js";
 const router = Router()
 
-router.post('/signUp' ,
-//  validation(validators.signUp),
- authController.signUp)
+router.post('/signUp' , validation(validators.signUp),authController.signUp)
 router.post('/login' , validation(validators.login), authController.login)
 router.get('/confirmEmail/:token' , validation( validators.token) ,authController.confirmEmail)
 router.get('/newConfirmEmail/:token' ,validation( validators.token) ,authController.confirmEmail)
